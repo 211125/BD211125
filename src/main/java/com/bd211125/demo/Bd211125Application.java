770b3583fe46862443ab62555e72b2331e70d6e8
211125 <84791176+211125@users.noreply.github.com>
2022-02-22 09:27:31 -0600

    commit unical

---
+++ src/main/java/com/bd211125/demo/Bd211125Application.java
@@ -0,0 +1,13 @@
+package com.bd211125.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd211125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd211125Application.class, args);
+	}
+
+}
