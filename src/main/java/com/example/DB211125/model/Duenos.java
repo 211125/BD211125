c892be0cf3ac3ce9c47735cd5b78d407c7568b9e
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2022-03-01 14:15:43 -0600

    Crea la tabla en MYSQL

--- src/main/java/com/example/DB211125/model/Duenos.java
+++ src/main/java/com/example/DB211125/model/Duenos.java
@@ -1,4 +1,50 @@
 package com.example.DB211125.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "duenos")
 public class Duenos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private Integer celular;
+
+    public void setCelular(Integer celular){
+        this.celular = celular;
+    }
+
+    public Integer getCelular(){
+        return celular;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
 }
