c56a073f1b84a1650780876e8bba9e894975c9dd
211125 <84791176+211125@users.noreply.github.com>
2022-03-01 10:18:11 -0600

    Add files via upload

---
+++ src/main/java/com/example/DB211125/Db211125Application.java
@@ -0,0 +1,13 @@
+package com.example.DB211125;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db211125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db211125Application.class, args);
+	}
+
+}
